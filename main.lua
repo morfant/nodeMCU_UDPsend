6824fa786bd498b1863e32d5d8ded9cc8b0f39fc
morfant <giy.hands@gmail.com>
2016-10-17 11:34:40 +0900

    Succeed sending UDP msg

--- main.lua
+++ main.lua
@@ -22,24 +22,26 @@ gpio.write(but_0, gpio.HIGH)
 
 
 print("Main.lua run!!")
-
+dest = net.createConnection(net.UDP,0)
+dest:connect(DEST_PORT, DEST_IP)
 
 -- print("dest: " .. dest)
--- dest:on("connection", function(sck, c)
---   -- Wait for connection before sending.
---     print("Connected to " .. DEST_IP)
--- 	tmr.alarm(0, (sensorInt * 100), 1, main)
--- end)
-
 function main()
+
+	-- dest = net.createConnection(net.UDP,0)
+	-- dest:connect(9001, "192.168.0.2")
+	-- tmr.delay(10000)
+	-- dest:send(but_0)
+	-- dest:send("im " .. but_0, function () print("data sent!") end)
+	-- print("im " .. but_0)
+	-- dest:close()
+	-- tmr.delay(1000000)
+
 	if gpio.read(but_0) == gpio.LOW then
 		gpio.write(3, gpio.LOW)
 		print("But_0(gpio 5) is LOW")
 		if msgSent_0 == false then
-			dest = net.createConnection(net.UDP,0)
-			dest:connect(DEST_PORT, DEST_IP)
-			dest:send("im " .. but_0)
-			dest:close()
+			dest:send(but_0)
 			msgSent_0 = true
 		end
 		-- pushInt = pushInt + 1;
@@ -47,7 +49,7 @@ function main()
 		-- print("pushInt: " .. pushInt)
 	else
 		-- pushInt = 0
-		-- gpio.write(3, gpio.HIGH)
+		gpio.write(3, gpio.HIGH)
 		msgSent_0 = false
 		print("But_0(gpio 5) is HIGH")
 	end
